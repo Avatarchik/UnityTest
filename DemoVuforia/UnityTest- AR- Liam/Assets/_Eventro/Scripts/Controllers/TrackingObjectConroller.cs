20c4c91bf0d09903c5b2cae33589af238ea3ab11
HarshdeepSingh <sandstargames@gmail.com>
2016-08-19 02:43:43 +0530

    - Cube Movement when object tracked

--- DemoVuforia/UnityTest- AR- Liam/Assets/_Eventro/Scripts/Controllers/TrackingObjectConroller.cs
+++ DemoVuforia/UnityTest- AR- Liam/Assets/_Eventro/Scripts/Controllers/TrackingObjectConroller.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class TrackingObjectConroller : MonoBehaviour {
 
+	public Vector3 initialPos;
+	public float speed = 5;
+	private bool foundStatus = false;
+
 	#region Init
 	// Use this for initialization
 	void Start () {
@@ -30,15 +34,19 @@ public class TrackingObjectConroller : MonoBehaviour {
 	void DefaultTrackableEventHandler_TrackerState (bool obj)
 	{
 		if (obj) { // Object Found
-			Debug.LogError ("Find");
+			foundStatus = true;
 		} else { // Object Lost
-			Debug.LogError ("Lost");
+			foundStatus = false;
 		}
 	}
 	#endregion
 
 	// Update is called once per frame
 	void Update () {
-
+		if (foundStatus) {
+			gameObject.transform.Translate (Vector3.up * speed * Time.deltaTime);
+		}else{
+			gameObject.transform.localPosition = initialPos;
+		}
 	}
 }
